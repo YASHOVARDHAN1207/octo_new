4dbaa235b9cca9765b49f8412bd5a7fa132162cc
Abhiram B.S.N <abhirambsn@gmail.com>
2022-11-05 19:09:02 +0530

    Frontend Initialized

---
+++ frontend/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Octo</title>
+        <meta name="description" content="Octo App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  )
+}
