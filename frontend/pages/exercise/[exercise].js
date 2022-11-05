c76af7d21b841b480f6697e9407e11f0f9d22ae2
distroinfinity <manurajput2911@gmail.com>
2022-11-06 02:57:55 +0530

    Exercise Page UI Complete

--- frontend/pages/exercise/[exercise].js
+++ frontend/pages/exercise/[exercise].js
@@ -1,9 +1,360 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
-const ExercisePage = () => {
+import { Pose } from "@mediapipe/pose";
+import * as cam from "@mediapipe/camera_utils";
+import Webcam from "react-webcam";
+import { useRef, useEffect, useState } from "react";
+import angleBetweenThreePoints from "./../../utils/angle";
+// import { Button } from "@material-ui/core"; /assets/images/bicepcurls.png
+import bicepcurls from "../../assets/img/bicepcurls.png";
+import crunches from "../../assets/img/crunches.png";
+import pushups from "../../assets/img/pushup.png";
+import squats from "../../assets/img/squats.png";
+// import { Link } from "react-router-dom";
+
+const styles = {
+  webcam: {
+    position: "absolute",
+    marginRight: "auto",
+    marginLeft: "auto",
+    left: 0,
+    right: 800,
+    top: 200,
+    textAlign: "center",
+    zIndex: 9,
+    width: 960,
+    height: 720,
+  },
+  countBox: {
+    position: "absolute",
+    marginRight: "auto",
+    marginLeft: "auto",
+    left: 1100,
+    right: 0,
+    top: 600,
+    width: 400,
+    height: 100,
+  },
+  selectBox: {
+    position: "absolute",
+    marginRight: "auto",
+    marginLeft: "auto",
+    left: 1000,
+    right: 0,
+    top: 250,
+    textAlign: "center",
+    width: 400,
+    color: "#05386B",
+    background: "#8EE4AF",
+  },
+  back: {
+    position: "absolute",
+    marginRight: "auto",
+    marginLeft: "auto",
+    left: 1700,
+    right: 0,
+    top: 850,
+  },
+};
+
+const exrInfo = {
+  bicepCurls: {
+    index: [12, 14, 16],
+    ul: 160,
+    ll: 50,
+  },
+  squats: {
+    index: [24, 26, 28],
+    ul: 170,
+    ll: 50,
+  },
+  pushups: {
+    index: [12, 14, 16],
+    ul: 160,
+    ll: 80,
+  },
+  crunches: {
+    index: [12, 24, 26],
+    ul: 130,
+    ll: 50,
+  },
+};
+
+let count = 0;
+let dir = 0;
+let angle = 0;
+function Counter(props) {
+  const [exr, setExr] = useState("");
+  const router = useRouter();
+
+  // const {exercise} = router.query
+
+  let imgSource = exr;
+  if (exr === "bicepCurls") {
+    imgSource = bicepcurls;
+  } else if (exr === "squats") {
+    imgSource = squats;
+  } else if (exr === "pushups") {
+    imgSource = pushups;
+  } else if (exr === "crunches") {
+    imgSource = crunches;
+  }
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  let camera = null;
+  const countTextbox = useRef(null);
+
+  function onResult(results) {
+    if (results.poseLandmarks) {
+      const position = results.poseLandmarks;
+
+      // set height and width of canvas
+      canvasRef.current.width = webcamRef.current.video.videoWidth;
+      canvasRef.current.height = webcamRef.current.video.videoHeight;
+
+      const width = canvasRef.current.width;
+      const height = canvasRef.current.height;
+
+      //ratios between 0-1, covert them to pixel positions
+      const upadatedPos = [];
+
+      const indexArray = exrInfo[exr].index;
+
+      for (let i = 0; i < 3; i += 1) {
+        upadatedPos.push({
+          x: position[indexArray[i]].x * width,
+          y: position[indexArray[i]].y * height,
+        });
+      }
+      angle = Math.round(angleBetweenThreePoints(upadatedPos));
+
+      // Count reps
+      //0 is down, 1 is up
+      if (angle > exrInfo[exr].ul) {
+        //console.log("test angle ",angle)
+        if (dir === 0) {
+          //count.current = count.current + 0.5
+          // console.log(count, " ", dir, " decrement ", angle);
+          dir = 1;
+        }
+      }
+      if (angle < exrInfo[exr].ll) {
+        if (dir === 1) {
+          count = count + 1;
+          // console.log(count, " ", dir, " increment ", angle);
+          dir = 0;
+        }
+      }
+
+      //console.log(count.current)
+      const canvasElement = canvasRef.current;
+      const canvasCtx = canvasElement.getContext("2d");
+      canvasCtx.save();
+
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      //canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height)
+
+      for (let i = 0; i < 2; i++) {
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(upadatedPos[i].x, upadatedPos[i].y);
+        canvasCtx.lineTo(upadatedPos[i + 1].x, upadatedPos[i + 1].y);
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = "white";
+        canvasCtx.stroke();
+      }
+      for (let i = 0; i < 3; i++) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(upadatedPos[i].x, upadatedPos[i].y, 10, 0, Math.PI * 2);
+        canvasCtx.fillStyle = "#AAFF00";
+        canvasCtx.fill();
+      }
+      canvasCtx.font = "40px aerial";
+      canvasCtx.fillText(angle, upadatedPos[1].x + 10, upadatedPos[1].y + 40);
+      canvasCtx.restore();
+    }
+  }
+
+  useEffect(() => {
+    console.log("rendered");
+
+    let imgSource = router.query;
+    setExr(imgSource.exercise);
+    count = 0;
+    dir = 0;
+    const pose = new Pose({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+      },
+    });
+    pose.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      minDetectionConfidence: 0.6,
+      minTrackingConfidence: 0.5,
+    });
+
+    pose.onResults(onResult);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          countTextbox.current.value = count;
+          await pose.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  });
+  function resetCount() {
+    count = 0;
+    dir = 0;
+  }
+
   return (
-    <div>ExercisePage</div>
-  )
+    // <div>
+    //   <div style={styles.selectBox}>
+    //     <h1>Bicep Curls</h1>
+    //     <img src={imgSource} width="300" alternate="bicepimage"></img>
+    //     <br></br>
+    //     <div style={{ top: 50 }}>
+    //       <h1>Count</h1>
+    //       <input
+    //         variant="filled"
+    //         ref={countTextbox}
+    //         value={count}
+    //         textAlign="center"
+    //         style={{ height: 50, fontSize: 40, width: 80 }}
+    //       />
+    //       <br></br>
+    //       <br></br>
+    //       <button
+    //         // style={{ top: 15 }}
+    //         // size="large"
+    //         // variant="contained"
+    //         // color="primary"
+    //         onClick={resetCount}
+    //       >
+    //         Reset Counter
+    //       </button>
+    //     </div>
+    //   </div>
+    //   <Webcam
+    //     ref={webcamRef}
+    //     style={{
+    //       position: "absolute",
+    //       marginRight: "auto",
+    //       marginLeft: "auto",
+    //       right: 20,
+    //       left: 600,
+    //       top: 50,
+    //       textAlign: "center",
+    //       zIndex: 9,
+    //       width: 800,
+    //       height: 600,
+    //     }}
+    //   />
+    //   <canvas
+    //     ref={canvasRef}
+    //     style={{
+    //       position: "absolute",
+    //       marginRight: "auto",
+    //       marginLeft: "auto",
+    //       right: 0,
+    //       left: 600,
+    //       top: 50,
+    //       textAlign: "center",
+    //       zIndex: 9,
+    //       width: 800,
+    //       height: 600,
+    //     }}
+    //   />
+    //   <div style={styles.back}>
+    //     {/* <Link to="/counter">
+    //       <button size="large" variant="contained" color="primary">
+    //         Back
+    //       </button>
+    //     </Link> */}
+    //   </div>
+    // </div>
+    <div className="flex items-start justify-around mt-10">
+      <div>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginRight: "auto",
+            marginLeft: "auto",
+            right: 20,
+            left: 600,
+            top: 50,
+            textAlign: "center",
+            zIndex: 9,
+            width: 800,
+            height: 600,
+          }}
+          // className="text-center  w-[800px] h-[720px] pb-32 absolute"
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginRight: "auto",
+            marginLeft: "auto",
+            right: 0,
+            left: 600,
+            top: 50,
+            textAlign: "center",
+            zIndex: 9,
+            width: 800,
+            height: 600,
+          }}
+          // className="text-center  w-[800px] h-[720px] pb-32 absolute"
+        />
+      </div>
+      <section className="flex flex-col mr-2 space-y-10 items-center text-white">
+        <h3 className="text-3xl">Bicep Curls</h3>
+        <img src={imgSource?.src} width={300} alt={exr} />
+        <div className="h-24 flex flex-col items-center space-y-4">
+          <input
+            variant="filled"
+            ref={countTextbox}
+            value={count}
+            textAlign="center"
+            // style={{
+            //   height: 50,
+            //   fontSize: 40,
+            //   width: 80,
+            //   color: "white",
+            //   background: "transparent",
+            // }}
+            className="text-white bg-transparent text-[40px] text-center"
+          />
+
+          <button
+            // style={{ top: 15 }}
+            // size="large"
+            // variant="contained"
+            // color="primary"
+            className=""
+            onClick={resetCount}
+          >
+            Reset Counter
+          </button>
+        </div>
+        <p className="italic text-white font-bold">
+          Try to mimic and hold the following pose.
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default ExercisePage
+export default Counter;
