6b73e34dc37e417d2dd15cdc431c0c6cd89f8f25
Shivanshu0512 <shivanshum512@gmail.com>
2025-05-02 13:03:35 +0530

    v\chatbot

---
+++ frontend/lib/openai.js
@@ -0,0 +1,10 @@
+// frontend/lib/openai.js
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default openai;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,6 +4,7 @@ import ExerciseCard from "../components/ExerciseCard";
 import SelectForm from "../components/Form";
 import HumanAvatar from "../components/Avatar";
 import HealthStats from "../components/HealthStats";
+import Chatbot from '../components/Chatbot';
 
 export default function Home() {
   return (
@@ -13,29 +14,33 @@ export default function Home() {
         <meta name="description" content="Octo App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        style={{ display: "flex", flexDirection: "column", paddingTop: "10%" }}
-      >
-        <div className="mr-auto ml-auto mt-auto mb-auto w-full ">
-          {/* Leaderboard Section (Currently in center only) */}
-          {/* Graphs and Sections (Column wise division when implemented ) */}
-          <h3 className="text-center text-[#e3ffa8] font-bold text-4xl ">
+
+      {/* Welcome Text */}
+      <div style={{ display: "flex", flexDirection: "column", paddingTop: "10%" }}>
+        <div className="mr-auto ml-auto mt-auto mb-auto w-full">
+          <h3 className="text-center text-[#e3ffa8] font-bold text-4xl">
             Welcome to Octo - A metaverse of Healthcare
           </h3>
         </div>
+
+        {/* Form Section */}
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            paddingTop: "10%",
+            paddingTop: "5%",
             paddingLeft: "20%",
             width: "50%",
           }}
         >
-          <SelectForm></SelectForm>
+          <SelectForm />
+        </div>
+
+        {/* Chatbot Section */}
+        <div className="p-8">
+          <Chatbot />
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
